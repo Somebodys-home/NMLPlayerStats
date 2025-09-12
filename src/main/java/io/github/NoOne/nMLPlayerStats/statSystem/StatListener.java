ebafde7799f328cab545f54aabe735b31a8e0a4d
NoOne_NoBody <hypixelnerd347@gmail.com>
2025-09-12 13:09:31 -0400

    moved bonus health listener

--- src/main/java/io/github/NoOne/nMLPlayerStats/statSystem/StatListener.java
+++ src/main/java/io/github/NoOne/nMLPlayerStats/statSystem/StatListener.java
@@ -5,6 +5,7 @@ import io.github.NoOne.nMLPlayerStats.profileSystem.ProfileManager;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class StatListener implements Listener {
     private ProfileManager profileManager;
@@ -17,5 +18,23 @@ public class StatListener implements Listener {
     public void resetStats(ResetStatsEvent event) {
         Player player = event.getPlayer();
         profileManager.saveAProfileToConfig(player);
+    }
+
+    @EventHandler
+    public void updateStatsOnPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        double bonusHealth = profileManager.getPlayerProfile(player.getUniqueId()).getStats().getBonusHealth();
+
+        player.setMaxHealth(20 + bonusHealth);
+    }
+
+    @EventHandler
+    public void updateStatsWhenChanged(StatChangeEvent event) {
+        Player player = event.getPlayer();
+        if (event.getStat().equals("bonushealth")) {
+            double bonusHealth = profileManager.getPlayerProfile(player.getUniqueId()).getStats().getBonusHealth();
+
+            player.setMaxHealth(20 + bonusHealth);
+        }
     }
 }
