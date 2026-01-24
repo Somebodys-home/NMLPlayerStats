f8b2b1ea6f5c991427a37149326f6038200b817a
NoOne_NoBody <hypixelnerd347@gmail.com>
2026-01-24 01:11:35 -0500

    i don fucked up ignore this commit

--- src/main/java/io/github/NoOne/nMLPlayerStats/profileSystem/ProfileListener.java
+++ src/main/java/io/github/NoOne/nMLPlayerStats/profileSystem/ProfileListener.java
@@ -1,7 +1,6 @@
 package io.github.NoOne.nMLPlayerStats.profileSystem;
 
 import io.github.NoOne.nMLPlayerStats.NMLPlayerStats;
-import io.github.NoOne.nMLPlayerStats.statSystem.Stats;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
